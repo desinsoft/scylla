d5a4209980c81702c85bd63bc056c5197b537092
jossenino <jossenino@gmail.com>
2017-10-24 19:30:11 -0300

    get someone credentials by user login
    get someone credentials by user login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,7 @@ import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import {Observable} from 'rxjs/Observable';
 
 import { Profile } from '../../models/profile';
+import { Credentials } from '../../models/credentials';
 import { CredentialsPage } from '../../pages/credentials/credentials';
 
 @Component({
@@ -16,7 +17,8 @@ export class HomePage {
 
   profileData: AngularFireObject<Profile>
   profiles: Observable<any>;
-  credentials: Observable<any>;
+  credentials: any;
+  credential = {} as Credentials;
 
   constructor(public navCtrl: NavController, 
     private auth: AngularFireAuth, 
@@ -31,7 +33,26 @@ export class HomePage {
         }).present();
         //console.log(this.dataBase.object(`profile/${data.uid}`));
         this.profiles = this.dataBase.object(`profile/${data.uid}`).valueChanges();
-        this.credentials = this.dataBase.object(`credential/${data.uid}`).valueChanges();
+        this.dataBase.list(`wallet/${data.uid}`).valueChanges().subscribe(res => {
+          console.log(res);
+        });
+       this.credentials = this.dataBase.list(`wallet/${data.uid}/credentials`).snapshotChanges().map(credentials => {
+        console.log(credentials.map(c => ({ key: c.payload.key, value: c.payload.val() })));
+        this.credentials = credentials.map(c => ({ key: c.payload.key, value: c.payload.val() }));
+        console.log(this.credentials);
+        for (var index = 0; index < this.credentials.length; index++) {
+          let values = {} as any;
+
+          values.account = this.credentials[index].value.account;
+          values.hash = this.credentials[index].value.hash;
+          values.password = this.credentials[index].value.password;
+          
+          console.log(values);
+          this.credential += values;
+        }
+        console.log(this.credentials);
+       });
+       console.log(this.credential);
         //this.profileData.query (profile.);
       }
       else{
